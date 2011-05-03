bf5e0058f9f78a17939473f7a63d7e8525a47100
Taylor Yelverton <taylor@synergy-solutions.biz>
2011-05-03 15:59:07 -0500

    Initialize refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lcef::Application.config.secret_token = 'ffcaafc1380f260591fca82c76a152a8691808a00d5770eb8ab588438e826dd613900f7dc800c517265da8e9e4bc82c55ff57dd8551d8b5e80791900b22aa9d0'
